99a741e8b4409d80db3b7fb48383023398d054d8
rohit-itobuz <rohit.bharti@itobuz.com>
2023-07-05 18:44:04 +0530

    to fix errors due to repeatation of operators

--- main.js
+++ main.js
@@ -1,15 +1,14 @@
 const calc = {
   symbols: ["+", "-", "&times;", "&div;", "%", "="], // array that contains the symbols
-  memory: [], // array that will store the numbers and symbols entered by the use
+  memory: [], // array that will store the numbers and symbols entered by the user
   screenMemory: "", // string that represents the current value displayed on the calculator screen.
 
   key: function (numb) {
     document.querySelector(".screen").innerHTML += numb;
-    this.screenMemory += "" + numb;
+    this.screenMemory += numb;
   },
   keySymbols: function (symbol) {
     const lastSymbol = this.memory[this.memory.length - 1];
-
     // Check if the last symbol is not in the symbols array
     // and the current symbol is not the same as the last symbol
     if (!this.symbols.includes(lastSymbol) && lastSymbol !== symbol) {
@@ -59,8 +58,8 @@ const calc = {
     for (let i = 0; i < this.memory.length; i++) {
       if (this.memory[i] === this.symbols[0]) {
         let res = Number(this.memory[i - 1]) + Number(this.memory[i + 1]);
-        if (res != res.toFixed(2)) {
-          const round = res.toFixed(2);
+        if (res != res.toFixed(3)) {
+          const round = res.toFixed(3);
           res = round;
         }
         document.querySelector(".screen").innerHTML = res;
@@ -68,26 +67,32 @@ const calc = {
         this.memory.push(res); // keep track of the calculated value for future operations if needed
       } else if (this.memory[i] === this.symbols[1]) {
         let res = Number(this.memory[i - 1]) - Number(this.memory[i + 1]);
-        if (res != res.toFixed(2)) {
-          const round = res.toFixed(2);
+        if (res != res.toFixed(3)) {
+          const round = res.toFixed(3);
           res = round;
         }
         document.querySelector(".screen").innerHTML = res;
         this.screenMemory = res;
         this.memory.push(res);
       } else if (this.memory[i] === this.symbols[2]) {
+        if (this.memory[i + 1] === "") {
+          this.memory[i + 1] = 1; // to fix errors due to repeatation of operators
+        }
         let res = Number(this.memory[i - 1]) * Number(this.memory[i + 1]);
-        if (res != res.toFixed(2)) {
-          const round = res.toFixed(2);
+        if (res != res.toFixed(3)) {
+          const round = res.toFixed(3);
           res = round;
         }
         document.querySelector(".screen").innerHTML = res;
         this.screenMemory = res;
         this.memory.push(res);
       } else if (this.memory[i] === this.symbols[3]) {
+        if (this.memory[i + 1] === "") {
+          this.memory[i + 1] = 1; // to fix errors due to repeatation of operators
+        }
         let res = Number(this.memory[i - 1]) / Number(this.memory[i + 1]);
-        if (res != res.toFixed(2)) {
-          const round = res.toFixed(2);
+        if (res != res.toFixed(3)) {
+          const round = res.toFixed(3);
           res = round;
         }
         document.querySelector(".screen").innerHTML = res;
@@ -95,8 +100,8 @@ const calc = {
         this.memory.push(res);
       } else if (this.memory[i] === this.symbols[4]) {
         let res = Number(this.memory[i - 1]) / 100;
-        if (res != res.toFixed(2)) {
-          const round = res.toFixed(2);
+        if (res != res.toFixed(3)) {
+          const round = res.toFixed(3);
           res = round;
         }
         document.querySelector(".screen").innerHTML = res;
